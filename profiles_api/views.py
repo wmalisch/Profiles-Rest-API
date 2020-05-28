b9962e8fd7e7cd35ad5f4251b990bcdf7608800d
Will Malisch <malisch.will@gmail.com>
2020-05-28 11:29:45 -0400

    Set up profile feed viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -116,3 +116,12 @@ class UserLoginApiView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
     
 
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading, and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+
+    def perform_create(self,serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
